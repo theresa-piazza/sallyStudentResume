a2e9a11c49a6087f690678a3b5f2a9dd6ef108c9
Theresa Piazza <tpiazza7867@gmail.com>
2020-08-06 11:28:32 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,49 @@
+function App(props) {
+    return (
+     <div className="App">
+     <main>
+         <section id="introduction">
+             <p>Hi, I'm Sally! I'm 25 and below this line you'll find more information about me.</p>
+         </section>
+ 
+          <section id='education'> 
+             "<p>I have studied at:</p>
+                 <ul>
+                 <li>IES BEZMILIANA</li>
+                 <li>Minnesota University</li>
+                 </ul>
+          </section>"
+ 
+         <section id="works">
+             <p>I have worked at:</p>
+             <div id="works_list">
+             <ul>
+                 <li>Github as Engineer during 2018 and there I have to
+                 Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                 </li>
+                 <li>Jetbrains as Project manager during 2019 and I'm still working there. My work consist in
+                 A assumenda autem dolorem doloremque
+                 </li>
+             </ul>
+             </div>
+         </section>
+ 
+         <section id="Contact-section" />
+         <p>My contact information: </p>
+             <address id="Contact-info">
+             <p>My name is Sally</p>
+             <p>My email is: <a href="mailto://sally@IntenetExplorer.old">sally@IntenetExplorer.old</a></p>
+             <p>My phone number is: <a href="tel:+34100572415">+34 100 57 24 15</a></p>
+             </address>
+         
+     </main>
+     </div>
+     
+     );
+ }
+
+ ReactDOM.render( 
+ <App />,
+ document.querySelector('#application-root')
+ );
+ 
